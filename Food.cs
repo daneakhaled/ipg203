95c8f279a5bc705e0514538bd72947c3de2610c2
daneakhaled <daneakh66@gmail.com>
2025-10-22 23:30:07 +0300

    Add files via upload

---
+++ Food.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace StoreSystem.Models
+{
+    public class Food : Product
+    {
+        public DateTime ExpiryDate { get; set; }
+
+        public Food(string id, string name, decimal price, DateTime expiryDate)
+            : base(id, name, price)
+        {
+            ExpiryDate = expiryDate;
+        }
+
+        public override void DisplayInfo()
+        {
+            Console.WriteLine($"[Food] {Name} - Expiry: {ExpiryDate.ToShortDateString()}, Price: {Price}$");
+        }
+
+        public override void Buy() => Console.WriteLine($"Buying Food: {Name}");
+        public override void Return() => Console.WriteLine($"Returning Food: {Name}");
+    }
+}
